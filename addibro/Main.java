f735b4ae9a05cb80afdf1104d617db7bf23641ff
Andreas Alme <andreas.alme.eriksson@gmail.com>
2019-01-11 13:10:46 +0100

    builder

--- addibro/Main.java
+++ addibro/Main.java
@@ -3,5 +3,13 @@ package addibro;
 public class Main{
     public static void main(String[] args) {
         Type factory = Factory.getFactory(Factory.Fac.BITCH);
+
+        System.out.println("=== BUILDER ===");
+        ClassToBuild ctb = new ClassToBuild.Builder().with(b -> {
+            b.field = "Hello";
+            b.field2 = 12;
+        }).build();
+
+        System.out.println(ctb);
     }
 }

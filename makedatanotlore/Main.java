ad434345675aff8a3ead4eec3b31600016e387d3
Andreas Alme <andreas.alme.eriksson@gmail.com>
2019-01-11 12:25:11 +0100

    Factory

--- makedatanotlore/Main.java
+++ makedatanotlore/Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-        Factory factory = Factory.getFactory(Factory.Fac.BITCH);
+        Type factory = Factory.getFactory(Factory.Fac.BITCH);
     }
 }

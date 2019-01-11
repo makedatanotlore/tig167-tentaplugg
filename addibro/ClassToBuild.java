15c3f4ca17ce1daa3b73a5e951d12f46547de157
Andreas Alme <andreas.alme.eriksson@gmail.com>
2019-01-11 12:45:20 +0100

    builder

---
+++ addibro/ClassToBuild.java
@@ -0,0 +1,28 @@
+import java.util.function.Consumer;
+
+/**
+ * Builder
+ */
+public class ClassToBuild {
+    private String field;
+    private int field2;
+
+    public OuterClass(String field, int field2) {
+        this.field = field;
+        this.field2 = field2;
+    }
+
+    public static class Builder {
+        public String field;
+        public int field2;
+
+        public Builder with(Consumer<Builder> b) {
+            b.accept(this);
+            return this;
+        }
+
+        public ClassToBuild build() {
+            return new OuterClass();
+        }
+    }
+}

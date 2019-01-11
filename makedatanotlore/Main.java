48df0de1265a541dbda4c250eba38939d0ef77d4
Andreas Alme <andreas.alme.eriksson@gmail.com>
2019-01-11 12:57:44 +0100

    changes

--- makedatanotlore/Main.java
+++ makedatanotlore/Main.java
@@ -1,9 +1,4 @@
 public class Main{
     public static void main(String[] args) {
-<<<<<<< HEAD
-
-=======
-        Type factory = Factory.getFactory(Factory.Fac.BITCH);
->>>>>>> ad434345675aff8a3ead4eec3b31600016e387d3
     }
 }
